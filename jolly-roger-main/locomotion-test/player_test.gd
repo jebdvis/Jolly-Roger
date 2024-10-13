8b4bcf536d5decd464e91f1f74a6568af313533c
Jeb Davis <jebemery@gmail.com>
2024-10-12 21:53:10 -0600

    added comments

--- jolly-roger-main/locomotion-test/player_test.gd
+++ jolly-roger-main/locomotion-test/player_test.gd
@@ -1,5 +1,6 @@
 extends CharacterBody3D
 
+#export variables that can be modified in properties
 #in degrees
 @export var max_steer_angle:float = 3.0
 @export var accel_decel_const:float = 1.0
@@ -13,16 +14,19 @@ var target_speed:float = 0.0
 var curr_speed: float = 0.0
 
 func _physics_process(delta: float) -> void:
+	#increase/decrease target speed based on player input of "w" and "a"
 	if Input.is_action_pressed("forward"):
 		target_speed = min(target_speed + (target_speed_change * delta), max_min_targ_speed)
 	elif Input.is_action_pressed("backward"):
 		target_speed = max(target_speed - (target_speed_change * delta), -max_min_targ_speed)
 		
+	#accels/decels to reach target speed
 	if target_speed > curr_speed:
 		curr_speed = min(curr_speed + (accel_decel_const * delta), target_speed)
 	elif target_speed < curr_speed:
 		curr_speed = max(curr_speed - (accel_decel_const * delta), target_speed)
 		
-		
+	#basic velocity calc
 	velocity = heading * curr_speed
+	
 	move_and_slide()
