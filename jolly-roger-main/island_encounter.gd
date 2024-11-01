299259d91013f2b68c674f83c23b9222695d3682
Jo-Nat-han <grandbuilder.jdo@gmail.com>
2024-11-01 17:56:42 -0600

    Refactored w/o use of player group

--- jolly-roger-main/island_encounter.gd
+++ jolly-roger-main/island_encounter.gd
@@ -1,6 +1,5 @@
 extends Area3D
 
 #island encounter detection
-func _on_body_entered(body):
-	if body.is_in_group("player"):
-		print("encounter!")
+func _on_body_entered(_body):
+	print("encounter!")
--- jolly-roger-main/island_interact.gd
+++ jolly-roger-main/island_interact.gd
@@ -1,6 +1,5 @@
 extends Area3D
 	
 #island interact detection
-func _on_body_entered(body):
-	if body.is_in_group("player"):
-		print("interact!")
+func _on_body_entered(_body):
+	print("interact!")
